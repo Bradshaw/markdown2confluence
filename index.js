bfbeec3a9f9ad5f16ed513168702ff59221ddf3c
Gaeel Bradshaw <gaeel.bradshaw@goto-games.com>
2021-01-05 15:14:54 +0100

    Render markdown

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const fs = require('fs');
+const MarkdownIt = require('markdown-it'),
+    md = new MarkdownIt();
 
 const inputs = {
   mdFile: "md-file",
@@ -13,8 +15,8 @@ try {
   // `who-to-greet` input defined in action metadata file
   const mdPath = core.getInput(inputs.mdFile);
   console.log(`Reading ${mdPath}`);
-  const content = fs.readFileSync(mdPath);
-  console.log(content);
+  const content = fs.readFileSync(mdPath, {encoding: "utf8"});
+  console.log(md.render(content));
   //const payload = JSON.stringify(github.context.payload, undefined, 2)
 } catch (error) {
   core.setFailed(error.message);
